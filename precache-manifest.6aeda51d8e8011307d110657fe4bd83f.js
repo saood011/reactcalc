af1c7c8e0f31f6260725204a78d268bc8fe71c3c
root <saood011@yahoo.com>
2019-11-06 10:51:05 +0100

    Updates

---
+++ precache-manifest.6aeda51d8e8011307d110657fe4bd83f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8172e1321a4cf4623ee54782ab74dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdb9c4a9bdf42e469cca",
+    "url": "/static/css/main.45ad8ee4.chunk.css"
+  },
+  {
+    "revision": "366ecb59073f5fbfd0f1",
+    "url": "/static/js/2.43ff30fc.chunk.js"
+  },
+  {
+    "revision": "bdb9c4a9bdf42e469cca",
+    "url": "/static/js/main.10fc6edc.chunk.js"
+  },
+  {
+    "revision": "ce3294b9d982cc93a2af",
+    "url": "/static/js/runtime-main.081b6741.js"
+  }
+]);
